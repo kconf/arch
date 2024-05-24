d0ac0e382e3e77dffe5b976ff8e88010d524c345
Jianwei Han <hanjianwei@gmail.com>
2024-05-24 13:10:29 +0800

    Fix script errors

--- install_binaries.sh
+++ install_binaries.sh
@@ -13,11 +13,11 @@ fi
 if [ ! -f $HOME/n/bin/n ]; then
   export NODE_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
   export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
-  "${SHELL}" < (curl -L https://bit.ly/n-install)
+  "${SHELL}" <(curl -L https://bit.ly/n-install)
 fi
 
 if [ ! -d $HOME/.cargo ]; then
   export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
   export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
-  "${SHELL}" < (curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)
+  "${SHELL}" <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)
 fi
